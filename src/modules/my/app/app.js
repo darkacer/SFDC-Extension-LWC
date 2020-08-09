625762b9b3bab5cb255bd250ec9bfc250823e759
darkacer <omkardeokar95@gmail.com>
2020-08-09 23:07:04 +0530

    show org names with formatting

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -4,19 +4,32 @@ import { LightningElement, track } from 'lwc';
 import { getOrgNames } from 'my/Utils';
 export default class App extends LightningElement {
     @track rows = [];
-    idToOrgObj = [];
+    // idToOrgObj = [];
 
     connectedCallback() {
+        let idToOrgObj = [];
+        let index = 0;
         getOrgNames((cookies) => {
             cookies.forEach((elem) => {
                 if (new RegExp('.*salesforce.com').test(elem.domain)) {
-                    this.idToOrgObj.push({
+                    idToOrgObj.push({
+                        index: index++,
                         value: elem.value,
-                        domain: elem.domain
+                        domain: elem.domain,
+                        class: 'white'
                     });
                 }
             });
-            this.rows = [...this.idToOrgObj];
+            this.rows = [...idToOrgObj];
         });
+    }
+
+    handleOrgSelect(event) {
+        let index = event.target.dataset.index;
+        console.log('you selected ', this.rows[index]);
+        this.rows.forEach((el) => {
+            el.class = 'white';
+        });
+        this.rows[index].class = 'green';
     }
 }
