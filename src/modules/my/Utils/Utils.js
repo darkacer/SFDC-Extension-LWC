b8c69b503380e76021a85995a732df759140d667
darkacer <omkardeokar95@gmail.com>
2020-08-09 20:45:59 +0530

    getting orgs name

--- src/modules/my/Utils/Utils.js
+++ src/modules/my/Utils/Utils.js
@@ -1,26 +1,13 @@
-let idToOrgInfo = new Map();
+/* eslint-disable no-undef */
+const myBrowser = typeof chrome === 'undefined' ? browser : chrome;
 
-const getOrgUrls = () => {
-    // eslint-disable-next-line no-undef
-    const myBrowser = typeof chrome === 'undefined' ? browser : chrome;
-
-    var count = 0;
-    myBrowser.cookies.getAll({ name: 'sid' }, function (sidcook) {
-        sidcook.forEach((elem) => {
-            var domain = elem.domain;
-            if (new RegExp('.*salesforce.com').test(domain)) {
-                console.log(elem);
-
-                idToOrgInfo.set(count, {
-                    value: elem.value,
-                    domain: elem.domain
-                });
-                count++;
-            }
-        });
-    });
-
-    return idToOrgInfo;
+const getOrgNames = (callback) => {
+    myBrowser.cookies.getAll(
+        {
+            name: 'sid'
+        },
+        callback
+    );
 };
 
-export { getOrgUrls };
+export { getOrgNames };
--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,8 +1,22 @@
-import { LightningElement } from 'lwc';
-import { getOrgUrls } from 'my/Utils';
-
+/* eslint-disable guard-for-in */
+/* eslint-disable vars-on-top */
+import { LightningElement, track } from 'lwc';
+import { getOrgNames } from 'my/Utils';
 export default class App extends LightningElement {
+    @track rows = [];
+    idToOrgObj = [];
+
     connectedCallback() {
-        console.log('getOrgUrls', getOrgUrls());
+        getOrgNames((cookies) => {
+            cookies.forEach((elem) => {
+                if (new RegExp('.*salesforce.com').test(elem.domain)) {
+                    this.idToOrgObj.push({
+                        value: elem.value,
+                        domain: elem.domain
+                    });
+                }
+            });
+            this.rows = [...this.idToOrgObj];
+        });
     }
 }
